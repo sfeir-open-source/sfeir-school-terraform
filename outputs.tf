640d86aceaafe25bc00b3c3b0c2caca14d483409
jnahelou <nahelou.j@sfeir.com>
2020-02-21 09:34:37 +0100

    Upgrade to 0.12 #24

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,5 @@
 output "instance_ip" {
   description = "Instance IP allocated by Google"
-  value       = "${google_compute_instance.tf_instance.network_interface.0.network_ip}"
+  value       = google_compute_instance.tf_instance.network_interface[0].network_ip
 }
-
 
