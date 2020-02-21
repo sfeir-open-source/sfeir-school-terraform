800538aa6f08f57d297fe8fb031486e9487186b7
jnahelou <nahelou.j@sfeir.com>
2020-02-21 09:27:19 +0100

    Upgrade to 0.12 #24

--- main.tf
+++ main.tf
@@ -6,5 +6,6 @@ resource "null_resource" "check-version" {
 
 output "message" {
   description = "A message to display"
-  value       = "${null_resource.check-version.triggers.message}"
+  value       = null_resource.check-version.triggers.message
 }
+
