179325e420c12b166e49670b2f49945378d9a6ee
Antoine Meausoone <antoine.meausoone@gmail.com>
2020-06-17 21:16:29 +0200

    feat(module-6): use helm3

--- install_vault.sh
+++ install_vault.sh
@@ -2,21 +2,13 @@
 
 echo "install helm"
 # installs helm with bash commands for easier command line integration
-curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
-# add a service account within a namespace to segregate tiller
-kubectl --namespace kube-system create sa tiller
-# create a cluster role binding for tiller
-kubectl create clusterrolebinding tiller \
-    --clusterrole cluster-admin \
-    --serviceaccount=kube-system:tiller
+curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get-helm-3 | bash
 
-echo "initialize helm"
-# initialized helm within the tiller service account
-helm init --service-account tiller
 # updates the repos for Helm repo integration
 helm repo update
 
-# add incubator
+# add helm repo
+helm repo add stable https://kubernetes-charts.storage.googleapis.com
 helm repo add incubator https://kubernetes-charts-incubator.storage.googleapis.com/
 helm repo update
 
@@ -24,7 +16,7 @@ helm repo update
 sleep 20
 
 # Install consul
-helm install --name consul-cluster stable/consul
+helm install consul-cluster stable/consul
 
 # Install vault
-helm install incubator/vault --name vault-cluster --set vault.dev=true --set vault.config.storage.consul.address="consul-cluster:8500",vault.config.storage.consul.path="vault" --set service.type="LoadBalancer" --set replicaCount=1
+helm install vault-cluster incubator/vault  --set vault.dev=true --set vault.config.storage.consul.address="consul-cluster:8500",vault.config.storage.consul.path="vault" --set service.type="LoadBalancer" --set replicaCount=1
